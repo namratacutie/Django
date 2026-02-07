f2e24b3db2f97cd0567635c540663c613bb936f4
Lawarna Aree <supremaree@gmail.com>
2026-02-07 21:33:11 +0545

    feat: initialize Django project, add Nammu.jpg, and configure static files directory.

--- myproject/settings.py
+++ myproject/settings.py
@@ -115,3 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#Added static files dir
+STATFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
