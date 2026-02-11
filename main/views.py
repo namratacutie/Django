ccaa85ca8365993f790ed8d5661e1a8be8ad0c63
Lawarna Aree <supremaree@gmail.com>
2026-02-11 14:46:57 +0545

    feat: Set up initial Django project URL configuration and main app views and URL files.

--- main/views.py
+++ main/views.py
@@ -2,11 +2,3 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(response):
-    return HttpResponse("Hi my name is Lawarna Aree")
-
-def about(respone):
-    return HttpResponse("This is my about page")
-
-def contact(response):
-    return HttpResponse("This is my contact page.")
--- myproject/urls.py
+++ myproject/urls.py
@@ -19,7 +19,4 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("main.urls")),
-    path('about/', include("main.urls")),
-    path('contact/', include("main.urls")),
 ]
